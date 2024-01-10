0662f6d0d771a75be2ada31f7b0982a583e76bbe
Pedro Alarcon <pedroa54@icloud.com>
2024-01-10 11:31:27 -0600

    adds phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+console.log('index.js is being executed');
+
+const mainElement = document.getElementById('main');
+console.log('mainElement:', mainElement);
+mainElement.remove();
+
+// Get a reference to the <h1> element in your HTML
+const headerElement = document.querySelector('h1');
+console.log('headerElement:', headerElement);
+
+// Define the newHeader variable and assign it the value of headerElement
+const newHeader = headerElement;
+console.log('newHeader:', newHeader);
+newHeader.textContent = "pedro is the champion";
